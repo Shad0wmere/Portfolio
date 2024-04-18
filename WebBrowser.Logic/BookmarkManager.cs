a784a25cb412dee3379b1d2f8a3fcbfe58a3da32
Shad0wmere <82961280+Shad0wmere@users.noreply.github.com>
2024-04-18 03:59:13 -0500

    Add files via upload

---
+++ WebBrowser.Logic/BookmarkManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using WebBrowser.Data.HistoryBookmarksDataSetTableAdapters;
+
+namespace WebBrowser.Logic
+{
+    public class BookmarkManager
+    {
+        //add bookmarks.
+        public static void AddItemBookmarks(BookmarkItem item)
+        {
+            
+            {
+                var adapter = new BookmarksTableAdapter();
+                adapter.Insert(item.Id, item.Title, item.URL);
+            }
+ 
+        }
+
+        //get bookmarks.
+        public static List<BookmarkItem> GetBookmarksItems()
+        {
+            var adapter = new BookmarksTableAdapter();
+            var results = new List<BookmarkItem>();
+            var rows = adapter.GetData();
+            foreach (var row in rows)
+            {
+                var item = new BookmarkItem();
+                item.Id = row.Id;
+                item.URL = row.URL;
+                item.Title = row.Title;
+                
+                results.Add(item);
+
+            }
+            return results;
+
+        }
+
+
+    }
+}
